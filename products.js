bcf0b67683a93f40002db31e0315c662a419f25b
a2zdorkary-727 <a2zdorkary@gmail.com>
2025-09-08 04:38:42 +0600

    Delete products.js

--- products.js
+++
@@ -1,7 +0,0 @@
-// products.js
-const products = [
-  { name: "MSI Modern 14", url: "msi-laptop.html" },
-  { name: "MSI Modern 15", url: "msi-laptop-page-2.html" },
-  { name: "MSI Gaming GF63", url: "msi-laptop-page-3.html" },
-  { name: "MSI Stealth 16", url: "msi-laptop-page-4.html" }
-];
